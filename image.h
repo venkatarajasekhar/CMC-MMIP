ad85bf82d20839855f2ab8f6e9a062e2aa80570c
Igor Munkin <munkin.igor@gmail.com>
2015-11-02 14:45:17 +0300

    Refactored Image container. Fixed error with uninitialised values

--- image.h
+++ image.h
@@ -2,76 +2,62 @@
 #define _IMAGE_H_
 
 #include <cstddef>
-#include <cmath>
-#include <complex>
+#include <cstring>
 
 /* 
  * Template for base image type
  */
 template<class PixelType>
-   class Image
-    {
-        private:
-            size_t __width;
-            size_t __height;
-            PixelType *__data;
-        public:
-            Image(size_t _width, size_t _height);
-            ~Image();
-            PixelType& operator() (size_t row, size_t col);
-            size_t width();
-            size_t height();
-            PixelType norm(size_t row, size_t col);
-    };
-
-/* 
- * Availible types of images
- */
-typedef Image<float> GrayscaleFloatImage;
+class Image
+{
+    private:
+        size_t _width;
+        size_t _height;
+        PixelType *_data;
+    public:
+        Image(size_t width, size_t height);
+        ~Image();
+        PixelType& operator() (size_t row, size_t col);
+        size_t width() const;
+        size_t height() const;
+};
 
 /*
  * Implementation of Image template
  */
 template <class PixelType>
-    Image<PixelType>::Image(size_t _width, size_t _height): __width(_width), __height(_height)
-    {
-        __data = new PixelType[__width * __height];
-    }
+Image<PixelType>::Image(size_t width, size_t height): _width(width), _height(height)
+{
+    _data = new PixelType[_width * _height];
+    if (!_data)
+        throw "Image: Insufficient memory to allocate raw data";
+    std::memset(_data, 0, _width * _height * sizeof(PixelType));
+}
 
 template <class PixelType>
-    Image<PixelType>::~Image()
-    {
-        delete[] __data;
-    }
+Image<PixelType>::~Image() { delete[] _data; }
 
 template <class PixelType>
-    PixelType& Image<PixelType>::operator() (size_t row, size_t col)
-    {
-        size_t i = row;
-        size_t j = col;
-        if (row < 0) i = 0;
-        if (row >= __height) i = __height - 1;
-        if (col < 0) j = 0;
-        if (col >= __width) j = __width - 1;
-        return __data[i * __width + j];
-    }
+size_t Image<PixelType>::width() const { return _width; }
 
 template <class PixelType>
-    size_t Image<PixelType>::width()
-    {
-        return __width;
-    }
+size_t Image<PixelType>::height() const { return _height; }
 
 template <class PixelType>
-    size_t Image<PixelType>::height()
-    {
-        return __height;
-    }
+PixelType& Image<PixelType>::operator() (size_t row, size_t col)
+{
+    size_t i = row;
+    size_t j = col;
+    if (row < 0) i = 0;
+    if (row >= _height) i = _height - 1;
+    if (col < 0) j = 0;
+    if (col >= _width) j = _width - 1;
+    return _data[i * _width + j];
+}
 
-template <class PixelType>
-    PixelType Image<PixelType>::norm(size_t row, size_t col)
-    {
-        return abs(__data[row * __width + col]);
-    }
+/*
+ * Availible types of images
+ */
+typedef Image<float> GrayscaleFloatImage;
 
 #endif /* _IMAGE_H_ */
