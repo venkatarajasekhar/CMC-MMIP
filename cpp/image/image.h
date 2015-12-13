d9eaaa027a6c84bdc34212680c1fa83bae66c875
Igor Munkin <munkin.igor@gmail.com>
2015-12-13 17:48:12 +0300

    Added copy and move constructors and assignment operators

--- cpp/image/image.h
+++ cpp/image/image.h
@@ -16,9 +16,14 @@ class Image
         size_t _height;
         T *_data;
     public:
+        Image();
         Image(const size_t width, const size_t height);
         Image(const size_t width, const size_t height, const uint8_t* data);
+        Image(const Image& image);
+        Image(Image&& image);
         ~Image();
+        Image& operator=(const Image& image);
+        Image& operator=(Image&& image);
         T& operator() (const size_t row, const size_t col);
         size_t width() const;
         size_t height() const;
@@ -27,6 +32,9 @@ class Image
 /*
  * Implementation of Image template
  */
+template <class T>
+Image<T>::Image(): _width(0), _height(0) { _data = nullptr; }
+
 template <class T>
 Image<T>::Image(const size_t width, const size_t height):
     _width(width), _height(height)
@@ -40,7 +48,24 @@ Image<T>::Image(const size_t width, const size_t height, const uint8_t* data):
     _width(width), _height(height)
 {
     _data = new T[_width * _height];
-    std::memcpy(_data, data, _width * _height);
+    std::memcpy(_data, data, _width * _height * sizeof(T));
+}
+
+template <class T>
+Image<T>::Image(const Image<T>& image):
+    _width(image._width), _height(image._height)
+{
+    _data = new T[_width * _height];
+    std::memcpy(_data, image._data, _width * _height * sizeof(T));
+}
+
+template <class T>
+Image<T>::Image(Image<T>&& image): _width(image._width), _height(image._height)
+{
+    _data = image._data;
+    image._data = nullptr;
+    image._width = 0;
+    image._height = 0;
 }
 
 template <class T>
@@ -51,6 +76,34 @@ size_t Image<T>::width() const { return _width; }
 
 template <class T>
 size_t Image<T>::height() const { return _height; }
+
+template <class T>
+Image<T>& Image<T>::operator=(const Image<T>& image)
+{
+    if (_data)
+        delete[] _data;
+    _width = image._width;
+    _height = image._height;
+    _data = new T[_width * _height];
+    std::memcpy(_data, image._data, _width * _height * sizeof(T));
+    return *this;
+}
+
+template <class T>
+Image<T>& Image<T>::operator=(Image<T>&& image)
+{
+    if (this != &image)
+    {
+        delete[] _data;
+        _width = image._width;
+        _height = image._height;
+        _data = image._data;
+        image._data = nullptr;
+        image._width = 0;
+        image._height = 0;
+    }
+    return *this;
+}
 
 template <class T>
 T& Image<T>::operator() (const size_t row, const size_t col)
